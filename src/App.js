5785177e98b08c002e08986083886a4129598baf
opticalfalicy <opticalfalicy@gmail.com>
2020-01-14 12:48:21 -0500

    replicated key size

--- src/App.js
+++ src/App.js
@@ -8,36 +8,61 @@ function App() {
       <div className='bg'>
       </div>
       <div className='keys'>
-        <div className='octave-block'>
+        {/* <div className='octave-block'>
         
+        </div> */}
+        <div className='key-block cE'>
+          <div className='w-keys'>
+        <item className='key-note C W'>
+         </item>
+        <item className='key-note D W'>
+         </item>
+        <item className='key-note E W'>
+         </item>
+          </div>
+        <div className='b-keys'>
+         <item className='key-note Df Bl'>
+          </item>
+         <item className='key-note Ef Bl'>
+          </item>
+        </div>
         </div>
         <div className='key-block fB fB-1'>
+          <div className='w-keys'>
           <item className='key-note F W'>
-          </item>
-          <item className='key-note Gf B'>
           </item>
           <item className='key-note G W'>
           </item>
-          <item className='key-note Af B'>
-          </item>
           <item className='key-note A W'>
-          </item>
-          <item className='key-note Bf B'>
           </item>
           <item className='key-note B W'>
           </item>
+
+          </div>
+          <div className='b-keys'>
+          <item className='key-note Gf Bl'>
+          </item>
+          <item className='key-note Af Bl'>
+          </item>
+          <item className='key-note Bf Bl'>
+          </item>
+          </div>
         </div>
         <div className='key-block cE'>
+          <div className='w-keys'>
         <item className='key-note C W'>
          </item>
-         <item className='key-note Df B'>
-          </item>
         <item className='key-note D W'>
          </item>
-         <item className='key-note Ef B'>
-          </item>
         <item className='key-note E W'>
          </item>
+          </div>
+        <div className='b-keys'>
+         <item className='key-note Df Bl'>
+          </item>
+         <item className='key-note Ef Bl'>
+          </item>
+        </div>
         </div>
         {/* <div className='key-block fB fB-2'>
           <item className='key-note F W'>
